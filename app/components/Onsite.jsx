7d4394721f0cfa5ebb587a1ab416779e6e9ae8fe
brian <bcameron1489@gmail.com>
2024-03-07 11:16:40 -0500

    Standardize item attributes within onsite functions

--- app/components/Onsite.jsx
+++ app/components/Onsite.jsx
@@ -19,8 +19,10 @@ export function trackAddedToCart(product) {
 let klaviyo = window.klaviyo || []
 let item = {
         Name: product.title,
+        ProductID: product.id.substring(product.id.lastIndexOf('/') + 1),
         ImageURL: product.selectedVariant.image.url,
         Handle: product.handle,
+        Brand: product.vendor,
         Price: product.selectedVariant.price.amount
       }
       klaviyo.push(['track', 'hydrogen ATC', item])
